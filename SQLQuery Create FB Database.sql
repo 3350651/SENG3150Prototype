ea1d61db532054c8c4943f97f4c7c900c812f608
Jordan14E <92713486+Jordan14E@users.noreply.github.com>
2023-05-08 19:06:25 +1000

    Groups working on branch

--- SQLQuery Create FB Database.sql
+++ SQLQuery Create FB Database.sql
@@ -18,6 +18,8 @@ FOR LOGIN fb1;
 GRANT SELECT, INSERT, UPDATE, DELETE 
 TO fb1;
 
+DROP TABLE IF EXISTS USERGROUPS;
+DROP TABLE IF EXISTS GROUPS;
 DROP TABLE IF EXISTS Flights;
 DROP TABLE IF EXISTS Price;
 DROP TABLE IF EXISTS Distances;
@@ -32,7 +34,7 @@ DROP TABLE IF EXISTS USERS;
 
 CREATE TABLE USERS
 (
-	userID			  UNIQUEIDENTIFIER,
+	userID			  CHAR(8),
 	first_name			VARCHAR(20),
 	last_name			VARCHAR(20),
 	email				VARCHAR(30),
@@ -164,13 +166,31 @@ CREATE TABLE Flights (
   CONSTRAINT FlightsStopOverCode_FK FOREIGN KEY (StopOverCode) REFERENCES Destinations (DestinationCode)
 )
 
+-- Create groups
+
+CREATE TABLE GROUPS
+(
+	groupID 	CHAR(8) PRIMARY KEY,
+	groupName		VARCHAR(20),
+	-- poolID
+	-- chatID
+	-- faveListID
+
+)
+CREATE TABLE USERGROUPS
+(
+	userGroupsID		CHAR(8),
+	userID	CHAR(8) FOREIGN KEY REFERENCES USERS(userID),
+	groupID CHAR(8) FOREIGN KEY REFERENCES GROUPS(groupID),
+	isAdmin		BIT DEFAULT(0),
+)
 
 go
 
-INSERT INTO USERS VALUES (NEWID(), 'Lachlan', 'ONeill', 'lachlan@gmail.com', 'lo', '04 123 456', 'user', '12 Main St, Carrington, NSW, Australia', 'Simple', 'AUD', '+10', 'Light', 0, '1997-08-29')
-INSERT INTO USERS VALUES (NEWID(), 'Jordan', 'Eade', 'jordan@gmail.com', 'je', '04 454 678', 'user', '45 Smith St, Brisbane, QLD, Australia', 'Simple', 'AUD', '+10', 'Light', 1, '2003-07-14');
-INSERT INTO USERS VALUES (NEWID(), 'Lucy', 'Knight', 'lucy@gmail.com', 'lk', '04 474 235', 'user', '87 George St, Adelaide, SA, Australia', 'Stripe', 'AUD', '+10', 'Dark', 0, '2004-03-21');
-INSERT INTO USERS VALUES (NEWID(), 'Blake', 'Baldin', 'blake@gmail.com', 'bb', '04 123 456', 'user', '39 High St, Melbourne, VIC, Australia', 'Recommend', 'AUD', '+10', 'Dark', 0, '2002-10-06')
+INSERT INTO USERS VALUES ('01010101', 'Lachlan', 'ONeill', 'lachlan@gmail.com', 'lo', '04 123 456', 'user', '12 Main St, Carrington, NSW, Australia', 'Simple', 'AUD', '+10', 'Light', 0, '1997-08-29')
+INSERT INTO USERS VALUES ('11112222', 'Jordan', 'Eade', 'jordan@gmail.com', 'je', '04 454 678', 'user', '45 Smith St, Brisbane, QLD, Australia', 'Simple', 'AUD', '+10', 'Light', 1, '2003-07-14');
+INSERT INTO USERS VALUES ('98765432', 'Lucy', 'Knight', 'lucy@gmail.com', 'lk', '04 474 235', 'user', '87 George St, Adelaide, SA, Australia', 'Stripe', 'AUD', '+10', 'Dark', 0, '2004-03-21');
+INSERT INTO USERS VALUES ('12345678', 'Blake', 'Baldin', 'blake@gmail.com', 'bb', '04 123 456', 'user', '39 High St, Melbourne, VIC, Australia', 'Recommend', 'AUD', '+10', 'Dark', 0, '2002-10-06')
 
 
 INSERT INTO Country (countryCode2, countryCode3, countryName, alternateName1, alternateName2, motherCountryCode3, motherCountryComment)
