e7acd1be15e5da5f3dee88af10fe27d5a7f75e0a
lyknight2 <104610404+lyknight2@users.noreply.github.com>
2023-05-28 16:49:44 +1000

    Added Calendar skeleton

---
+++ src/main/java/startUp/CalendarBean.java
@@ -0,0 +1,26 @@
+package startUp;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class CalendarBean implements Serializable {
+    private String calendarID;
+    private Timestamp startDate;
+    private Timestamp endDate;
+    private String userID;
+    private String groupID;
+
+    public CalendarBean(){}
+
+    public String getCalendarID() {
+        return calendarID;
+    }
+
+    public Timestamp getStartDate() {
+        return startDate;
+    }
+
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+}
